0926989405b65bedc9f972f82d7b5d0923cbbd3b
edwftw <plo.erd@gmail.com>
2020-04-19 01:57:06 -0300

    Removind database drop after init and unused imports.

--- src/main/java/org/demo/interfaceadapters/controller/CustomerController.java
+++ src/main/java/org/demo/interfaceadapters/controller/CustomerController.java
@@ -3,8 +3,6 @@ package org.demo.interfaceadapters.controller;
 import org.demo.entity.Customer;
 import org.demo.usecase.CustomerUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
