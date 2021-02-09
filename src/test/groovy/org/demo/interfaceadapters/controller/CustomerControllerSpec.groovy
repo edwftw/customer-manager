e51cd46159b9835ede1f55de469402a014c2d87c
Paulo Eduardo da Silva Soares <psoares@ciandt.com>
2021-02-09 13:47:11 -0300

    Removing solution tests.

--- src/test/groovy/org/demo/interfaceadapters/controller/CustomerControllerSpec.groovy
+++ src/test/groovy/org/demo/interfaceadapters/controller/CustomerControllerSpec.groovy
@@ -1,78 +1,7 @@
 package org.demo.interfaceadapters.controller
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import org.demo.entity.Customer
-import org.demo.usecase.CustomerUseCase
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.http.MediaType
-import org.springframework.test.web.servlet.MockMvc
 import spock.lang.Specification
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
-
-@WebMvcTest(CustomerController.class)
 class CustomerControllerSpec extends Specification {
 
-    @MockBean
-    CustomerUseCase customerUseCase
-
-    @Autowired
-    ObjectMapper objectMapper
-
-    @Autowired
-    MockMvc mockMvc
-
-    def "should get customers" () {
-
-        customerUseCase.getCustomers() >> getCustomersMock()
-
-        when: "end point is called"
-        def response = mockMvc.perform(get("/customers")).andReturn()
-
-        then: "response "
-        response.response.status == 200
-    }
-
-    def "should post customer" () {
-
-        Customer customer = new Customer()
-        customer.setFirstName("First Name")
-        customer.setLastName("Last Name")
-        customer.setAge(30)
-
-        when: "end point is called"
-        def response = mockMvc.perform(post("/customer")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(getCustomerJson(customer)))
-                        .andReturn()
-
-        then: "response "
-        response.response.status == 200
-
-    }
-
-    def getCustomersMock() {
-        Customer customer = new Customer()
-        customer.setId("123")
-        customer.setFirstName("First Name")
-        customer.setLastName("Last Name")
-        customer.setAge(30)
-
-        List<Customer> customers = new ArrayList<>()
-        customers.add(customer)
-
-        return customers
-    }
-
-    def getCustomerJson(Customer customer) {
-        try {
-            return new ObjectMapper().writeValueAsString(customer);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
--- src/test/groovy/org/demo/usecase/CustomerUseCaseSpec.groovy
+++ src/test/groovy/org/demo/usecase/CustomerUseCaseSpec.groovy
@@ -1,24 +1,7 @@
 package org.demo.usecase
 
-import org.demo.entity.Customer
-import org.demo.interfaceadapters.database.mongo.CustomerRepository
 import spock.lang.Specification
 
 class CustomerUseCaseSpec extends Specification {
 
-    CustomerRepository customerRepository = Mock()
-
-    CustomerUseCase customerUseCase = new CustomerUseCase(customerRepository)
-
-    def "Should save a customer" () {
-        given: "A valid customer"
-        Customer customer = new Customer()
-        customer.setLastName("lastName")
-
-        when: "customer use case is called"
-        customerUseCase.save(customer)
-
-        then: "repository must be called"
-        1 * customerRepository.save(customer)
-    }
 }
