1c729a56c0a8dffc9ff0547b6f68d8b46f6a6f35
Paulo Eduardo da Silva Soares <psoares@ciandt.com>
2021-02-10 16:18:00 -0300

    Removing solution

--- src/test/groovy/org/demo/usecase/ValidateDriverEligibilityUseCaseSpec.groovy
+++ src/test/groovy/org/demo/usecase/ValidateDriverEligibilityUseCaseSpec.groovy
@@ -1,37 +1,7 @@
 package org.demo.usecase
 
 import spock.lang.Specification
-import spock.lang.Unroll
 
 class ValidateDriverEligibilityUseCaseSpec extends Specification {
 
-     ValidateDriverEligibilityUseCase useCase = new ValidateDriverEligibilityUseCase()
-
-    @Unroll
-    def "Should return #scenario" () {
-        given: "a age"
-        Integer age = ageMock
-
-        when: "the use case is called"
-        boolean result = useCase.execute(age)
-
-        then: "the age must be validated"
-        result == expected
-
-        where: "with parameters"
-        scenario    | ageMock | expected
-        "valid"     | 20      | true
-        "invalid"   | 18      | false
-    }
-
-    def "Should return exception" () {
-        given: "a age"
-        Integer age = null
-
-        when: "the use case is called"
-        useCase.execute(age)
-
-        then: "the age must be validated"
-        thrown(Exception)
-    }
 }
